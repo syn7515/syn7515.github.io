9bd0c85deb31cabdd2ac2badb8cf42bf8433e570
Park Sooyeon <heejin7515@gmail.com>
2021-04-04 23:57:17 +0900

    initial commit
    done until video upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import ReactPlayer from 'react-player/lazy'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className='player-wrapper'>
+        <ReactPlayer
+          className='react-player'
+          url='https://www.dailymotion.com/video/x80dy7b' loop='true' playing='true' muted = 'true'
+          width='100%'
+          height='100%'
+        />
+      </div>
+      </div>
     </div>
   );
 }
