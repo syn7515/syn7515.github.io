e0ef6a9de9e7e6e36d0c63252a7504ef5cf8506a
Park Sooyeon <heejin7515@gmail.com>
2021-04-05 01:49:32 +0900

    gothub publishing
    published to
    https://syn7515.github.io

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ function App() {
     <div className="App">
       <div>
         <div className='player-wrapper'>
-        <ReactPlayer
-          className='react-player'
-          url='https://www.dailymotion.com/video/x80dy7b' loop='true' playing='true' muted = 'true'
-          width='100%'
-          height='100%'
+        <ReactPlayer className='react-player'
+          url='https://www.dailymotion.com/video/x80dy7b' 
+          loop='true' playing='true' muted = 'true'
+          width='100%' height='100%'
         />
       </div>
       </div>
