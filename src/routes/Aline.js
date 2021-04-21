c52000122fab15b48a920aa7359fd98e3997ca6a
Park Sooyeon <heejin7515@gmail.com>
2021-04-22 02:38:00 +0900

    aline updated

--- src/routes/Aline.js
+++ src/routes/Aline.js
@@ -18,7 +18,7 @@ function Aline() {
             <p className = 'text-body'><br/>Aline is a ESG Rovo-advisor based financial service focusing on individual’s governing value. In addition to earning profit through investment, users can help chaning the world positively by supporting corporates that are in align with their value.  </p>
         </div>
         <div className='container'>
-            <Grid container spacing={3}>
+            <Grid container spacing={3} direction='row' justify="center" alignItems="flex-start">
                 <Grid item xs ={6} sm={3}>
                     <div className='text-subtitle'>Output</div>
                     <div className='text-body'>Service Design <br/>Mobile application</div>
@@ -34,8 +34,8 @@ function Aline() {
                         <a href="https://ifworlddesignguide.com/search?q=nh%2Binvestment&search=nh%20investment#/pages/page/entry/312630-aline" target="_blank" rel="noopener noreferrer">IF Award 2021</a></div>
                 </Grid>
                 <Grid item xs ={6} sm={3}>
-                    <div className='text-subtitle'>Output</div>
-                    <div className='text-body'>Service Design <br/>Mobile application</div>
+                    <div className='text-subtitle'>Duration</div>
+                    <div className='text-body'>4 months (2020.03 - 2020.06)</div>
                 </Grid>
             </Grid>
         </div>
@@ -83,7 +83,39 @@ function Aline() {
                     <div className='text-phrase-explanation' style={{marginBottom : 75}}>Beyond profit-making, Aline delivers value and the impact of money. By gathering the data from the initial survey and regular value updates, the index is weighted according to the user’s value. Based on index, the investment portfolio will be adjusted and consumption againtst the value will be ararmed.</div>
                 </div>
             </div>
-            <img className="master-image" src={process.env.PUBLIC_URL + '/assets/aline/solution.png'} alt="solution"/>
+            <div className='container'>
+                <Grid container spacing={3} direction='row' justify="center" alignItems="flex-start">
+                <Grid item xs ={6} sm={4} lg={2}>
+                    <img className="master-image" src={process.env.PUBLIC_URL + '/assets/aline/journey1.png'} alt="user journey 1"/>
+                    <div className='text-journey'>Start Aline</div>
+                </Grid>
+                <Grid item xs ={6} sm={4} lg={2}>
+                    <img className="master-image" src={process.env.PUBLIC_URL + '/assets/aline/journey2.png'} alt="user journey 2"/>
+                    <div className='text-journey'>Set Personal values</div>
+                </Grid>
+                <Grid item xs ={6} sm={4} lg={2}>
+                    <img className="master-image" src={process.env.PUBLIC_URL + '/assets/aline/journey3.png'} alt="user journey 3"/>
+                    <div className='text-journey'>Start Investment based on personal values</div>
+                </Grid>
+                <Grid item xs ={6} sm={4} lg={2}>
+                    <img className="master-image" src={process.env.PUBLIC_URL + '/assets/aline/journey4.png'} alt="user journey 4"/>
+                    <div className='text-journey'>Check the financial and social impact</div>
+                </Grid>
+                <Grid item xs ={6} sm={4} lg={2}>
+                    <img className="master-image" src={process.env.PUBLIC_URL + '/assets/aline/journey5.png'} alt="user journey 5"/>
+                    <div className='text-journey'>Update the portfolio thorugh value update</div>
+                </Grid>
+                <Grid item xs ={6} sm={4} lg={2}>
+                    <img className="master-image" src={process.env.PUBLIC_URL + '/assets/aline/journey6.png'} alt="user journey 6"/>
+                    <div className='text-journey'>Check company’s value during payment</div>
+                </Grid>
+                </Grid>
+            </div>
+            <div className='container' style={{marginTop:270,  marginBottom : 0}}>
+                <div className='text-subtitle-white'>Personal Value Index (PVI)</div>
+                <div className='text-body' style={{color : 'white'}}>Referring to MSCI ratings, we created value index system with 6 main criteria. The personal value index adjusts the investment portfolio an evaluate consumptions. <a href="https://naver.com" target='_blank' rel="noopener noreferrer" style={{color : 'white'}}>Learn more</a> </div>
+                <img className="master-image" src={process.env.PUBLIC_URL + '/assets/aline/pvi.png'} alt="personal value index diagram" style={{marginTop:90}} />
+            </div>
         </div>
     </div>);
 }
