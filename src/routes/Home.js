072c48d184ec80c90f2bef83ce8cfa8c68a56bbc
Park Sooyeon <heejin7515@gmail.com>
2021-06-01 01:35:34 +0900

    minor design update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -31,12 +31,12 @@ function Home()  {
             <Grid container className='grid-container' direction='row' justify='space-between' alignItems='flex-start'>
                 <Divider classes={{root : classes.divider}} orientation="vertical" flexItem='true' style={{marginLeft:"-1px"}}/>
                 <Grid item className='grid-item' xs={12} sm={4}>
-                        <div className='text-category'>Sooyeon Park</div>
+                        <div className='text-category'>SOOYEON PARK</div>
                         <div className='text-main' >I am a senior student studying <a href='https://id.kaist.ac.kr' target="_blank" rel="noopener noreferrer" className='a_diff'> Industrial Design at KAIST</a>, interested in architecture and programming.</div>
                 </Grid>
                 <Divider classes={{root : classes.divider}} orientation="vertical" flexItem='true' style={{marginRight:"-1px"}}/>
                 <Grid item className='grid-item' xs={12} sm={4}>
-                    <div className='text-category'>project</div>
+                    <div className='text-category'>PROJECTS</div>
                     <Grid container direction='row'>
                         <Grid item xs={6}>
                             
@@ -59,7 +59,7 @@ function Home()  {
                 </Grid>
                 <Divider classes={{root : classes.divider}} orientation="vertical" flexItem='true' style={{marginRight:"-1px"}}/>
                 <Grid item className='grid-item' xs={12} sm={4} style={{marginBottom : 50}}>
-                    <div className='text-category'>experience</div>
+                    <div className='text-category'>EXPERIENCE</div>
                     <Grid container direction='column' spacing={3}>
                         <Grid item><div className='text-main'>Internship at  <a href='https://ux.snu.ac.kr' target="_blank" rel="noopener noreferrer" className='a_diff'>UX lab</a><br/>2020.09 - 2020.10<br/>Research Assistant</div></Grid>
                         <Grid item><div className='text-main'>Internship at  Pavilion corp. <br/>2020.01 - 2020.02<br/>UI/UX design and branding</div></Grid>
